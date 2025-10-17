464f26e2dcfa21938b4ca4ef432ecf8655f4ef07
byaurea <aureacarminato@gmail.com>
2025-10-16 21:23:27 -0300

    improvement/card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 export default function Card({title, price, description, image, rating}) {
 
     return(
-        <div>
-            <img src={image} alt="" />
+        <div className="border-1 rounded-2xl p-2">
+            <img src={image} alt="" className="border-b-1 pt-5 pb-5"/>
             <div>
-                <h1>{title}</h1>
+                <h1 className="font-medium">{title}</h1>
                 <p>{price}</p>
                 <div>
-                    
+
                 </div>
             </div>
         </div>
